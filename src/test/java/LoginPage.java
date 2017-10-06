f4887791acb6f99ac44e3731a4084efc9144411d
Rafał Tyszewski <rafal.tyszewski@play.pl>
2017-10-06 10:27:50 +0200

    pageFactory dodane do page object

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -4,25 +4,32 @@ import java.util.logging.Logger;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoginPage {
+	
+	public LoginPage(){
+		//https://www.guru99.com/page-object-model-pom-page-factory-in-selenium-ultimate-guide.html
+        PageFactory.initElements(browserDriver.getCurrentDriver(), this);
+
+	}
 
     private static final Logger LOG = Logger.getLogger(LoginPage.class.getName());
 	
-	//@FindBy (id="LoginPage")
-	//private WebElement loginPageDiv;
+	@FindBy (id="LoginPage")
+	private WebElement loginPageDiv;
 	
-//	@Autowired
-//	BrowserDriver browserDriver;
+	@Autowired
+	BrowserDriver browserDriver;
 	
 	
 	public  void isDisplayedCheck(){
 	    LOG.info("********************Checking login page is displayed*******************");
-//	    browserDriver.waitForElement(loginPageDiv);
-	  //  loginPageDiv.isDisplayed();
+	    browserDriver.waitForElement(loginPageDiv, 10);
+	    loginPageDiv.isDisplayed();
 	}
 	
 }
--- src/test/java/Raz.java
+++
@@ -1,8 +0,0 @@
-package test.java;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Raz {
-
-}
